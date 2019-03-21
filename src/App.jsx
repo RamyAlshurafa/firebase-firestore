555f69534d6438abbe916cee88796cf35f07454c
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-03-21 14:09:57 +0200

    fix eslint warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import { addNewDish } from './firebase/functions';
+
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
-
+    addNewDish();
     return (
       <div className="App">
-      App
+        <Home />
       </div>
     );
   }
