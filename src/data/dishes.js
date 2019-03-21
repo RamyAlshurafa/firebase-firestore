eaf9a100ec84fc85df85da33e1444c86114e80a3
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-03-22 00:34:56 +0200

    create dishes initial data

---
+++ src/data/dishes.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    name: 'cooked meat and sauce platter',
+    url: 'https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+    rate: 3,
+  },
+  {
+    name: 'pancake',
+    url: 'https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    rate: 4,
+  },
+  {
+    name: 'salmon dish',
+    url: 'https://images.unsplash.com/photo-1552058091-1a614f470415?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=805&q=80',
+    rate: 5,
+  },
+  {
+    name: 'biscuits with frutes',
+    url: 'https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+    rate: 4,
+  }, {
+    name: 'garden salad',
+    url: 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    rate: 5,
+  }, {
+    name: 'pizza',
+    url: 'https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+    rate: 5,
+  },
+];
